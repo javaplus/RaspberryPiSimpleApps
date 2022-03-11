f07813a0d22b020a529c4a6fac0931a5ee18edb7
Barry <btarlton@gmail.com>
2022-03-10 20:26:05 -0500

    Update countLine.py handle exit (needs refactored)

--- countLine.py
+++ countLine.py
@@ -20,12 +20,16 @@ display.fill(0)
 display.print(0)
 ##### END SETUP ####
 
-
 tripped_time_sec = 0
 is_tripped = False
+tripped_time_sec_exit = 0
+is_tripped_exit = False
+EXIT_LED = 17
+
 GPIO.setwarnings(False)    # Ignore warning for now
 GPIO.setmode(GPIO.BCM)   # Use physical pin numbering
 GPIO.setup(4, GPIO.OUT, initial=GPIO.LOW)   # Set pin 8 to be an output pin and set initial value to low (off)
+GPIO.setup(EXIT_LED, GPIO.OUT, initial=GPIO.LOW)   # Set pin 8 to be an output pin and set initial value to low (off)
 
 def show_count():
   global count
@@ -36,10 +40,19 @@ def light_on():
   print("light up!!!!!!!!!!!!!!!!!!!")
   GPIO.output(4, GPIO.HIGH) # Turn on
 
-
 def light_off():
   print("light off??????")
   GPIO.output(4, GPIO.LOW) # Turn on
+
+def exit_light_on():
+  print("light up!!!!!!!!!!!!!!!!!!!")
+  GPIO.output(EXIT_LED, GPIO.HIGH) # Turn on
+
+
+def exit_light_off():
+  print("light off??????")
+  GPIO.output(EXIT_LED, GPIO.LOW) # Turn on
+
 
 
 count = 0
@@ -51,10 +64,11 @@ def entered_line():
   print("count=" + str(count))
   show_count()
 
-def exiting_line():
-  light_off()
+def exited_line():
+  exit_light_on()
   global count
   count = count - 1
+  show_count()
 
 def check_entrance():
     global is_tripped
@@ -67,10 +81,8 @@ def check_entrance():
        tripped_time_sec = tripped_time_sec + time.time()
        if(tripped_time_sec > 2):
          entered_line()
-         # Reset counter so we don't count the same person twice
-         tripped_time_sec = 0
-         # Pause a second to let them pass
-         time.sleep(1)
+         # Reset is_tripped so we don't count same person multiple times
+         is_tripped = False
      else:
        tripped_time_sec = 0
        is_tripped = True
@@ -80,11 +92,36 @@ def check_entrance():
      is_tripped = False
      light_off()
 
+
+def check_exit():
+    global is_tripped_exit
+    global tripped_time_sec_exit
+    voltage = adc.get_adc(1)
+    print("ADC Channel 1:", round(voltage, 2), "V")
+    if(voltage < 2.3):
+     # see if tripped for more than 2 seconds
+     if(is_tripped_exit):
+       tripped_time_sec_exit = tripped_time_sec_exit + time.time()
+       if(tripped_time_sec_exit > 2):
+         exited_line()
+         # Reset flag so we don't count the same person twice
+         is_tripped_exit = False
+     else:
+       tripped_time_sec_exit = 0
+       is_tripped_exit = True
+    # else not tripped (not dropped below 2.3v
+    else:
+     tripped_time_sec_exit = 0
+     is_tripped_exit = False
+     exit_light_off()
+
 try:
 # Loop continually to check sensors
   while True:
     check_entrance()
+    check_exit()
     time.sleep(0.2)
 
 except KeyboardInterrupt:
   GPIO.cleanup()
+
